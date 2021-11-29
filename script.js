707a31de2efe67c6e38d5d927f8b359621696177
viigneshreddy <68388801+viigneshreddy@users.noreply.github.com>
2021-11-29 16:44:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var selectedRow = null
-
+var btn = document.getElementById("myBtn");
+var modal = document.getElementById("myModal");
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
@@ -32,8 +33,8 @@ function insertNewRecord(data) {
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.city;
     cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
+    cell4.innerHTML = `<button class="btn1"><a class ="a1" onClick="onEdit(this)">Edit</a></button>
+                      <button class="btn2"> <a class ="a2"onClick="onDelete(this)">Delete</a></button>`;
 }
 
 function resetForm() {
@@ -45,7 +46,9 @@ function resetForm() {
 }
 
 function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
+  modal.style.display = "none";
+    
+    selectedRow = td.parentElement.parentElement.parentElement;
     document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
     document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
     document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
@@ -60,7 +63,7 @@ function updateRecord(formData) {
 
 function onDelete(td) {
     if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
+        row = td.parentElement.parentElement.parentElement;
         document.getElementById("employeeList").deleteRow(row.rowIndex);
         resetForm();
     }
@@ -70,10 +73,46 @@ function validate() {
     if (document.getElementById("fullName").value == "") {
         isValid = false;
         document.getElementById("fullNameValidationError").classList.remove("hide");
+
     } else {
         isValid = true;
         if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
             document.getElementById("fullNameValidationError").classList.add("hide");
     }
     return isValid;
+}
+// Get the modal
+
+
+// Get the button that opens the modal
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+
+
+// var btn1 = document.getElementsByClassName("a1");
+var showModal = document.getElementById("showModal")
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+showModal.onclick = function() {
+    modal.style.display = "block";
+  }
+// btn1.onclick = function() {
+//     modal.style.display = "none";
+//   }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
